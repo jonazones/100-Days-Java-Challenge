0784e75dd014dfa6d6ac2e420b1d4168c8d14b5f
JonaZones <jonatha.da.silva.2002@gmail.com>
2025-10-30 22:34:10 -0300

    first commit

---
+++ src/Day1.java
@@ -0,0 +1,14 @@
+public class Day1 {
+    public static void main(String[] args) {
+
+        System.out.println("Hello World");
+
+        // JavaChallenge
+
+        /*
+
+        100-Days-Java-Challenge
+
+         */
+    }
+}
